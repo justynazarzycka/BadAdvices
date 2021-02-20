aad6cf2b486ebaf97d6939b7ee36eb7636d1da44
Justyna Zarzycka <justyna.zarzycka@gmail.com>
2021-02-20 23:06:13 +0100

    Initial Commit

---
+++ BadAdvices/BadAdvicesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BadAdvicesApp.swift
+//  BadAdvices
+//
+//  Created by Justyna Zarzycka on 20/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct BadAdvicesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
